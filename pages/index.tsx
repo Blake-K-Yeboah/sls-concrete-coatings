d4ca2b583166796eab089a95e656cdb0e3bbd91c
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-18 22:00:20 +1000

    Added Header Component To Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+// Page Components
+import Header from "../components/Home/Header";
+
 const Home: NextPage = () => {
     return (
         <>
             <Head>
                 <title>SLS Concrete Coatings - Home</title>
+                <meta charSet="utf-8" />
             </Head>
-            <h1>Home</h1>
+            <Header />
         </>
     );
 };
