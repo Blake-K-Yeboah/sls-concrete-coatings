c4a091c565afa391754efcbc6e866d53d92bd451
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-18 22:00:09 +1000

    Added Layout Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+// Stylesheet
 import "../styles/globals.sass";
+
+// Types
 import type { AppProps } from "next/app";
 
+// Components
+import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
