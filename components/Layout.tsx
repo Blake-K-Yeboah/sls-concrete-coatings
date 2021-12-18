c3bd5f43800bd64902a3d23c3d6d6ede999ce7de
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-18 22:01:47 +1000

    Created Layout Component

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import Navbar from "./Navbar";
+
+const Layout: FC = ({ children }) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+        </>
+    );
+};
+
+export default Layout;
