72033dafb1211cc33e54ff6b69bbafe8b2a6a98c
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-19 22:22:36 +1000

    Created LinkBoxSection Component

---
+++ components/Home/LinkBoxSection.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+
+import LinkBox from "./LinkBox";
+
+import styles from "../../styles/LinkBoxes.module.sass";
+
+const LinkBoxSection: FC = () => {
+    const linkBoxes = [
+        {
+            text: "Get A Free Quote",
+            href: "/contact-us",
+            imgId: "1",
+        },
+        {
+            text: "About Our Products",
+            href: "/our-products",
+            imgId: "2",
+        },
+        {
+            text: "View Photo Gallery",
+            href: "/photo-gallery",
+            imgId: "3",
+        },
+    ];
+
+    return (
+        <section className={styles.linkBoxGrid}>
+            {linkBoxes.map((link) => (
+                <LinkBox link={link} key={link.imgId} />
+            ))}
+        </section>
+    );
+};
+
+export default LinkBoxSection;
