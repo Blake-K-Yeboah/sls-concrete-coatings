82d2a3ae7cb60b16b049a0b12e883bcd750ddc3d
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-19 22:22:07 +1000

    Created Title Component

---
+++ components/Common/Title.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+// Props Interface
+interface IProps {
+    text: string;
+    type: "left" | "center"; // Alignment
+}
+
+const Title: FC<IProps> = ({ text, type }) => {
+    return (
+        <>
+            <h2 className={`section-title ${type}`}>{text as string}</h2>
+            <div className="section-divider"></div>
+        </>
+    );
+};
+
+export default Title;
