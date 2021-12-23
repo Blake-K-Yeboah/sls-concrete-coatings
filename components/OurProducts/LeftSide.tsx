b7c68234ed85eb5c8d96ed054dfaf66c7a8d5765
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-23 15:29:04 +1000

    Created LeftSide Component

---
+++ components/OurProducts/LeftSide.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { FC } from "react";
+
+import styles from "../../styles/OurProducts.module.sass";
+
+import Title from "../Common/Title";
+
+const LeftSide: FC = () => {
+    return (
+        <div className={styles.leftSide}>
+            <Title type="left" text="Spray Paving" />
+            <p className={styles.paragraph}>
+                Our product is a specially formulated coating system that
+                adheres permanently to concrete giving the appearance of a newly
+                laid decorative driveway, patio, pool surround or garage.
+            </p>
+            <div>
+                <Link href="/our-products/spray-paving">
+                    <a className={`btn primary ${styles.firstBtn}`}>
+                        Learn More
+                    </a>
+                </Link>
+                <Link href="/gallery">
+                    <a className="btn light-outline">View Photo Gallery</a>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default LeftSide;
