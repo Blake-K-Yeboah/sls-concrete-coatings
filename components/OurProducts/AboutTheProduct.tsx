361348b390632f61db5dd657b338e3c26e9ff196
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-24 12:36:10 +1000

    Created AboutTheProduct Component

---
+++ components/OurProducts/AboutTheProduct.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import styles from "../../styles/OurProducts.module.sass";
+
+const AboutTheProduct: FC = () => {
+    return (
+        <div className={styles.aboutTheProduct}>
+            <h1>About the product</h1>
+        </div>
+    );
+};
+
+export default AboutTheProduct;
