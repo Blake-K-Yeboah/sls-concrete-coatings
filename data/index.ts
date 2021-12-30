b04e63b59abbe4e71f919e15ac48b44fe3d18270
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-30 15:40:22 +1000

    Added applications data

---
+++ data/index.ts
@@ -0,0 +1,14 @@
+export const applications: string[] = [
+    "Driveways",
+    "Patios",
+    "Shops",
+    "Factories",
+    "Pool Surrounds",
+    "Amenities Blocks",
+    "Garages",
+    "Stairs",
+    "Sheds",
+    "Rumpus Rooms",
+    "Kitchens",
+    "Laundries",
+];
