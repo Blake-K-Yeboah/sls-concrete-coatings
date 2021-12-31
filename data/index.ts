39dd4ed6995522a9a44c62e8c6e570b4bb013ee1
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-31 16:53:47 +1000

    Added data for colour-chart page

--- data/index.ts
+++ data/index.ts
@@ -12,3 +12,207 @@ export const applications: string[] = [
     "Kitchens",
     "Laundries",
 ];
+
+export const sprayPavingColours = [
+    {
+        name: "white",
+        color: "#fff",
+    },
+    {
+        name: "lightstone",
+        color: "#E4E3D9",
+    },
+    {
+        name: "sandstone",
+        color: "#E3DDA9",
+    },
+    {
+        name: "buff",
+        color: "#DABB78",
+    },
+    {
+        name: "beige",
+        color: "#D1B68B",
+    },
+    {
+        name: "rust",
+        color: "#BB8A2E",
+    },
+    {
+        name: "cinnamon",
+        color: "#AB8449",
+    },
+    {
+        name: "tan",
+        color: "#774E10",
+    },
+    {
+        name: "salmon",
+        color: "#DABFA2",
+    },
+    {
+        name: "coffee",
+        color: "#88735D",
+    },
+    {
+        name: "mango",
+        color: "#C8872B",
+    },
+    {
+        name: "terracotta",
+        color: "#9E5520",
+    },
+    {
+        name: "brick red",
+        color: "#98362D",
+    },
+    {
+        name: "heritage green",
+        color: "#005B56",
+    },
+    {
+        name: "stormy",
+        color: "#376080",
+    },
+    {
+        name: "marine blue",
+        color: "#2A70AE",
+    },
+    {
+        name: "purple",
+        color: "#3C3C92",
+    },
+    {
+        name: "plum",
+        color: "#7D6F68",
+    },
+    {
+        name: "oyster",
+        color: "#A6A6A6",
+    },
+    {
+        name: "blue gum",
+        color: "#939389",
+    },
+    {
+        name: "blue stone",
+        color: "#696A6C",
+    },
+    {
+        name: "silver grey",
+        color: "#696A6C",
+    },
+    {
+        name: "charcoal",
+        color: "#4A545D",
+    },
+    {
+        name: "black",
+        color: "#272526",
+    },
+];
+
+export const epoxyFloorsRanges = {
+    standard: [
+        {
+            name: "butter scotch",
+            imgNum: 1,
+        },
+        {
+            name: "cappuccino",
+            imgNum: 2,
+        },
+        {
+            name: "emerald",
+            imgNum: 3,
+        },
+        {
+            name: "ghost gum",
+            imgNum: 4,
+        },
+        {
+            name: "majestic blue",
+            imgNum: 5,
+        },
+        {
+            name: "midnight",
+            imgNum: 6,
+        },
+        {
+            name: "oatmeal",
+            imgNum: 7,
+        },
+        {
+            name: "peppercorn",
+            imgNum: 8,
+        },
+        {
+            name: "ruby",
+            imgNum: 9,
+        },
+        {
+            name: "sandy beach",
+            imgNum: 10,
+        },
+    ],
+    stoneLook: [
+        {
+            name: "cappuccino",
+            imgNum: 11,
+        },
+        {
+            name: "ghostgum",
+            imgNum: 12,
+        },
+        {
+            name: "majestic blue",
+            imgNum: 13,
+        },
+        {
+            name: "midnight",
+            imgNum: 14,
+        },
+    ],
+    designer: [
+        {
+            name: "alpine snow",
+            imgNum: 15,
+        },
+        {
+            name: "cream brulee",
+            imgNum: 16,
+        },
+        {
+            name: "gold dust",
+            imgNum: 17,
+        },
+        {
+            name: "honeycomb",
+            imgNum: 18,
+        },
+        {
+            name: "arctic ice",
+            imgNum: 19,
+        },
+        {
+            name: "luna grey",
+            imgNum: 20,
+        },
+        {
+            name: "oyster shell",
+            imgNum: 21,
+        },
+        {
+            name: "salt & pepper",
+            imgNum: 22,
+        },
+        {
+            name: "galaxy way",
+            imgNum: 23,
+        },
+        {
+            name: "night sky",
+            imgNum: 24,
+        },
+    ],
+};
