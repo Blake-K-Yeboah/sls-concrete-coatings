744c5644b25b212d6f5a664964b9a19c497d5ec0
Blake Yeboah <blakekyeboah@gmail.com>
2021-12-31 16:55:18 +1000

    Created SprayPavingColours Component

---
+++ components/ColourChart/SprayPavingColours.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+// Styling
+import styles from "../../styles/ColourChart.module.sass";
+
+// Data
+import { sprayPavingColours } from "../../data";
+
+const SprayPavingColours: FC = () => {
+    return (
+        <div className={styles.colourGrid}>
+            {sprayPavingColours.map((color) => {
+                return (
+                    <div className={styles.colourBoxContainer}>
+                        <div
+                            className={styles.colourBox}
+                            style={{ backgroundColor: color.color }}
+                        ></div>
+                        <p className={styles.colourName}>{color.name}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default SprayPavingColours;
