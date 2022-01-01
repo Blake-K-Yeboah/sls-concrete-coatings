8e1bf6d41719e2f3180c4991541f3ed735baca30
Blake Yeboah <blakekyeboah@gmail.com>
2022-01-01 22:33:47 +1000

    Added questions and answers for FAQ page

--- data/index.ts
+++ data/index.ts
@@ -216,3 +216,63 @@ export const epoxyFloorsRanges = {
         },
     ],
 };
+
+export const questionsAndAnswers = [
+    {
+        question: "What do you use to clean the epoxy? ",
+        answer: "`Epoxy floors are easy to clean. A neutral PH cleaner will suffice - depending on the requirements of the product and application used (whether in a commercial or residential area).",
+    },
+    {
+        question: "Why is an epoxy floor good for a garage?",
+        answer: "Unprotected concrete floors in garages wear out, become fouled by oils, liquids and damaged by certain corrosive elements. Epoxy floor coating systems protect concrete from wear and tear because it is harder and more durable than the concrete itself. A seamless epoxy floor provides a hard-wearing and long-lasting surface that prevents oils and liquids from seeping into the surface, drawing it in and staining it.",
+    },
+    {
+        question: "What are the advantages of Seamless Epoxy Floors?",
+        answer: "Seamless – No joints – Regardless of the size of the floor <br> Durable – Very strong than the natural concrete surface Chemical & Acid <br> Resistance – Excellent resistance to most acids and chemicals <br> Various textures – The finish can be installed with any texture you desire, from very smooth to textured for non-skid surface <br> Sanitary – Seamless and non-porous. There is no place for dirt or bacteria to accumulate and grow <br> Easy to maintain – Seamless and non-porous, dirt remains on the surface can be easily cleared away <br> Decorative – Solid colours or thousands of colour combinations",
+    },
+    {
+        question: "What are seamless floors?",
+        answer: "Seamless floors are floors without seams, ridges, or joints. They are poured-in-place epoxy, urethane, or methyl methacrylate formulations and they become a natural part of a building's very structure.",
+    },
+    {
+        question: "What are the advantages of epoxy?",
+        answer: "Epoxy is the most common choice for industrial and commercial settings because it is easy to use and provides a smooth finish.  However, due to its many benefits, Epoxy is increasingly used in residential environments—flooring and garage flooring.  One of the many benefits of epoxy is that it is seamless and impervious.  In large industrial buildings, it serves to protect the concrete underneath",
+    },
+    {
+        question: "What is an epoxy flake system?",
+        answer: "Flake Epoxy systems are an alternative flooring solution to tiles, vinyl, hardwood, laminate and carpets. The flakes/chips give a layer of protection to the concrete surface while also providing a seamless, long-lasting, sanitary, aesthetic, and low-maintenance flooring option. <br><br> Garages, kitchens, living and dining rooms, restrooms, and entire homes are all common places to use flake systems. These systems can be used in practically any outdoor application and provide long-lasting colour and attractiveness. <br><br> Check out colour ideas for some inspiration.",
+    },
+    {
+        question: "What’s the process for applying the epoxy floor?",
+        answer: "The concrete surface is grinded back to open the pores of the concrete and allow the epoxy to penetrate into the surface. <br> A sealer is applied to the ground concrete <br> A base colour coat is applied (flakes are added to the base coat if desired) <br> 2 coats of Polyurethane clear are added for protection and sleek finish",
+    },
+    {
+        question:
+            "I have many cracks and pits in my concrete floor, can they be repaired?",
+        answer: "We always repair all the cracks and pits of your floor. First we diamond grind the floor, then repair any imperfections in the concrete before laying the epoxy.",
+    },
+    {
+        question: "Do you offer warranty?",
+        answer: "Yes, every installation we do comes with a 5-year warranty with reasonable usage terms and conditions.",
+    },
+    {
+        question: "How durable is epoxy?",
+        answer: "When fully cured, epoxy floors have a flexural strength of more than 10,000psi which is stronger than concrete. However, with the addition of the polyurethane topcoat, it increases the strength and durability even more.",
+    },
+    {
+        question: "What is a polyurethane coating?",
+        answer: "A polyurethane coating is a polyurethane layer applied to the surface of a substrate for the purpose of protecting it. These coatings help protect substrates from various types of defects such as corrosion, discoloration, weathering, abrasion, and other deteriorating processes while also making the epoxy floor appear even smoother and shinier.",
+    },
+    {
+        question: "What are the benefits of Spray Paving?",
+        answer: "From driveways, and pathways to patios and pool surrounds, our Spray paving services can be used just about anywhere. This highly sought-after product offers benefits such as its slip resistant technology, soft texture, longevity and easy to maintain. It’s ideal for driveways as it prevents cars from sliding while also enhancing your driveways looks. With a wide variety of designs and colours to choose from, you can add value and love to your home by creating the perfect look.",
+    },
+    {
+        question: "What is Spray Paving?",
+        answer: "Spray Paving is a method of applying a decorative coating or finish to a normal concrete surface that mimics the appearance of traditional pavement techniques, transforming it into an attractive, durable surface. This type of pavement can be applied to both old and new concrete surfaces and is typically used for sidewalks, driveways and roads.",
+    },
+    {
+        question: "How is Spray Pave applied?",
+        answer: "The initial stage in spray paving is to clean the surface with a high-pressure cleaner. Then, to prevent overspray when the spray pave is being applied, surrounding areas such as walls and other floor surfaces must be covered. <br> Before a base coat is placed throughout the entire surface, any cracks in the concrete will be rectified. After that, the spray pave is applied evenly across the entire surface area, producing in a high-quality concrete stenciling outcome. <br> After removing the stencil, two coats of clear sealer are applied to the surface to seal it in.",
+    },
+];
