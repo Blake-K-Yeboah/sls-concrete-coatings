3457dec6b1978a512fca28d0ae5fb1075b2d3ee7
Blake Yeboah <blakekyeboah@gmail.com>
2022-01-03 15:53:26 +1000

    Fixed Bug When Changing Pages On Mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -115,6 +115,7 @@ const Navbar: FC = () => {
                                             ? styles.activeLink
                                             : ""
                                     }
+                                    onClick={toggleMobileMenu}
                                 >
                                     {link.text as string}
                                 </a>
