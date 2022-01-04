457993e1b3a176e9189f995c91cbc09924029cd8
Blake Yeboah <blakekyeboah@gmail.com>
2022-01-04 18:35:53 +1000

    Initialized AOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,22 @@ import type { AppProps } from "next/app";
 // Components
 import Layout from "../components/Layout";
 
+// useEffect Hook
+import { useEffect } from "react";
+
+// Animate On Scroll
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        AOS.init({
+            easing: "ease-out-cubic",
+            once: true,
+            offset: 50,
+        });
+    }, []);
+
     return (
         <Layout>
             <Component {...pageProps} />
