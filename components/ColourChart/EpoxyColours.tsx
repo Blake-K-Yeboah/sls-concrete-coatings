3b2568547a597162b11576fa1aebc1bf777012bf
Blake Yeboah <blakekyeboah@gmail.com>
2022-09-09 18:25:11 +1000

    Updated Naming Of Epoxy Colous

--- components/ColourChart/EpoxyColours.tsx
+++ components/ColourChart/EpoxyColours.tsx
@@ -16,7 +16,7 @@ const EpoxyColours: FC = () => {
                     data-aos="flip-down"
                     data-aos-duration="1000"
                 >
-                    COLOUR FLAKE™ Epoxy Flake Flooring
+                    Epoxy Flooring - Flake Samples
                 </summary>
 
                 <div className={styles.imgRange}>
@@ -47,38 +47,7 @@ const EpoxyColours: FC = () => {
                     data-aos="flip-down"
                     data-aos-duration="1000"
                 >
-                    DURASTONE™ 1.0 & 2.0 Epoxy Flake Flooring
-                </summary>
-
-                <div className={styles.imgRange}>
-                    {epoxyFloorsRanges.two.map((floor) => {
-                        return (
-                            <div
-                                className={styles.floorBoxContainer}
-                                key={floor.name + floor.imgNum}
-                            >
-                                <div className={styles.floorBoxImgContainer}>
-                                    <Image
-                                        src={`/images/colours/epoxy-floors/new/2/Image (${floor.imgNum}).jpg`}
-                                        layout="fill"
-                                        alt="Epoxy Floor"
-                                    />
-                                </div>
-                                <p className={styles.floorBoxTitle}>
-                                    {floor.name}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            </details>
-            <details>
-                <summary
-                    className={styles.rangeHeading}
-                    data-aos="flip-down"
-                    data-aos-duration="1000"
-                >
-                    Metallic Art Epoxy EP100® 100% Solid Epoxy
+                    Metallic Epoxy - Base Coat &amp; Pigment Samples
                 </summary>
 
                 <div className={styles.imgRange}>
@@ -109,7 +78,7 @@ const EpoxyColours: FC = () => {
                     data-aos="flip-down"
                     data-aos-duration="1000"
                 >
-                    EPOXY EP200™ 100% Solids - Epoxy Flooring
+                    Epoxy - Base Coat Samples
                 </summary>
                 <div className={styles.imgRange}>
                     {epoxyFloorsRanges.four.map((floor) => {
